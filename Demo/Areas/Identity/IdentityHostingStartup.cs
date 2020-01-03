ee429ee4db24e975cd3b9f58b885996a4cbef4cb
wunianqing <wunianqing@outlook.com>
2020-01-03 09:27:10 +0800

    Add project files.

---
+++ Demo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Demo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Demo.Areas.Identity.IdentityHostingStartup))]
+namespace Demo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AccountContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AccountContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AccountContext>();
+            });
+        }
+    }
+}
